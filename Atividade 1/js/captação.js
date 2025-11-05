fe06eeaf04eb5bc60a4fc752551d36b2ec9b8c28
João Marcos <joaomarcos0550@gmail.com>
2025-11-04 21:26:27 -0300

    Initial commit

---
+++ Atividade 1/js/captação.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('donation-form');
+  const progressFill = document.getElementById('progress-fill');
+  const progressMeta = document.getElementById('progress-meta');
+
+  let total = 0;
+  const meta = 1000;
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const valor = Number(
+      form.amount_custom.value ||
+      (form.amount.value ? form.amount.value : 0)
+    );
+
+    total += valor;
+    const porcentagem = Math.min((total / meta) * 100, 100);
+    progressFill.style.width = `${porcentagem}%`;
+    progressMeta.textContent = `R$ ${total} arrecadados de R$ ${meta} (${Math.round(porcentagem)}%)`;
+
+    alert(`Obrigado pela doação de R$${valor.toFixed(2)}!`);
+  });
+});
